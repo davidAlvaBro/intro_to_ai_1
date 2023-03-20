fe316b9ce1aa89ad507671703ec8510b614e3bbc
Lauge Hermansen <lauge.hermansen@hotmail.com>
2023-03-20 14:20:59 +0100

    hej

--- game_engine/test_stuff.py
+++ game_engine/test_stuff.py
@@ -1,41 +1,8 @@
-import config
+import pandas as pd
 
-from pieces import King, Switch, Defender, Deflector, Laser
-from gui import print_board
-import game
+a = [1,2,3,4,5,6,7,8,9,10]
 
-from board import Board
+# pandas save as csv
+df = pd.DataFrame(a)
+df.to_csv('test.csv', index=False)
 
-board = Board()
-
-snapshot = board.take_snapshot()
-
-
-def modify_board(board):
-# action = dict(position = (4,4), direction = config.Move.UP)
-
-
-    piece = board.board[4,3].contents
-
-
-    board.board[4,2].contents = piece
-    board.board[4,3].contents = None
-    piece.position = 4,2
-
-    return board
-
-def print_laser(board: Board):
-    for p in board.red_pieces:
-        if isinstance(p, Laser):
-            print(p.position, "red")
-    for p in board.blue_pieces:
-        if isinstance(p, Laser):
-            print(p.position, "blue")
-
-print_board(board)
-mod_baord = modify_board(board)
-print_board(board)
-print_laser(board)
-board.restore_from_snapshot(snapshot)
-print_laser(board)
-print_board(board)
